2d0f8c050a0a994b2d3e933752da3a7b48001722
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-05-05 20:51:12 +0500

    Added check for arg non undefined

--- src/common/checkSerializable.ts
+++ src/common/checkSerializable.ts
@@ -1,6 +1,9 @@
 import { ReflectMetaDataKeys } from './ReflectMetaDataKeys'
 
 export default function checkSerializable(target: Object): boolean {
+  if (!target) {
+    return false
+  }
   const options = Reflect.getMetadata(
     ReflectMetaDataKeys.TsJacksonSerializable,
     target
