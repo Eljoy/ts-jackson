278b674c84add7197140a9e2f64cd51c6e1af0c4
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-05-06 21:01:07 +0500

    Created ReflectMetaDataKeys enum

---
+++ src/common/ReflectMetaDataKeys.ts
@@ -0,0 +1,6 @@
+export enum ReflectMetaDataKeys {
+  TsJacksonJsonProperty = 'ts-jackson:jsonproperty',
+  TsJacksonSerializable = 'ts-jackson:serializable',
+  DesignType = 'design:type',
+  DesignParamTypes = 'design:paramtypes',
+}
