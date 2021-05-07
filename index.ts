70986f92939afa2bd0dab813c4756b67c76765fa
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-05-07 22:08:38 +0500

    added exports to root index

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { default as Serializable } from "./src/Serializable";
+export { default as deserialize } from "./src/deserialize";
+export { default as serialize } from "./src/serialize";
+export { default as JsonProperty } from "./src/JsonProperty";
