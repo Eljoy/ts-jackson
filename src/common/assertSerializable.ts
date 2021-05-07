3560c8a99a8ecbeda1e930142c488d725006cafb
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-05-07 22:09:10 +0500

    Provided better type than Object

--- src/common/assertSerializable.ts
+++ src/common/assertSerializable.ts
@@ -1,7 +1,9 @@
 import checkSerializable from './checkSerializable'
 import SerializableError from './SerializableError'
 
-export default function assertSerializable(target: Object) {
+export default function assertSerializable(
+  target: (new (...args: unknown[]) => unknown) | Function
+) {
   if (!checkSerializable(target)) {
     throw new SerializableError(target)
   }
