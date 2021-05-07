91789ba6a50a95bee7a9abea4d6c029aa51c59e4
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-05-07 22:07:03 +0500

    Minor change

--- src/common/SerializableError.ts
+++ src/common/SerializableError.ts
@@ -1,8 +1,9 @@
 import Serializable from '../Serializable'
 
 export default class SerializableError extends Error {
-  constructor(target: Object) {
-    const message = `${target} should annotated with ${Serializable.name} decorator`
-    super(message)
+  constructor(target: (new (...args: unknown[]) => unknown) | Function) {
+    super()
+    this.name = 'SerializableError'
+    this.message = `${target.name} class should annotated with @${Serializable.name} decorator`
   }
 }
