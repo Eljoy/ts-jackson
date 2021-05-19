c4dbdc8b64c22ff3a655de75218992551d3ead8d
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2021-05-19 21:59:48 +0500

    Added SerializableEntity export

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export { default as Serializable } from "./src/Serializable";
 export { default as deserialize } from "./src/deserialize";
 export { default as serialize } from "./src/serialize";
 export { default as JsonProperty } from "./src/JsonProperty";
+export { default as SerializableEntity } from "./src/SerializableEntity";
