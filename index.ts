afa161bd45677de4213ed601de7760b170a4f6e4
iliasgazdaliev <ilias@candis.io>
2026-07-27 18:04:33 +0200

    Export reusable naming strategies: camelToSnakeCase, camelToKebabCase, camelToPascalCase

--- index.ts
+++ index.ts
@@ -7,6 +7,11 @@ export {
 export { default as deserialize } from './src/deserialize'
 export { default as deserializeArray } from './src/deserializeArray'
 export { default as JsonProperty } from './src/JsonProperty'
+export {
+  camelToKebabCase,
+  camelToPascalCase,
+  camelToSnakeCase,
+} from './src/namingStrategies'
 export { default as Serializable } from './src/Serializable'
 export { default as SerializableEntity } from './src/SerializableEntity'
 export { default as serialize } from './src/serialize'
